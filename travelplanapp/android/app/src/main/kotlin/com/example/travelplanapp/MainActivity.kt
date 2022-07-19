cbc532544b45d2826440573f706a1fa94b846957
linecut20 <80865818+linecut20@users.noreply.github.com>
2022-07-19 11:17:37 +0900

    init commit

---
+++ travelplanapp/android/app/src/main/kotlin/com/example/travelplanapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.travelplanapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ travelplanapp/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TravelPlanApp());
+}
+
+class TravelPlanApp extends StatelessWidget {
+  const TravelPlanApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+      ),
+      body: const Center(
+        child: Text('여행앱을 만들어요오'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [],
+      ),
+    );
+  }
+}
