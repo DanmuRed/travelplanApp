7091412f884ad658c5a8a586ac4abe5fceccd842
linecut20 <80865818+linecut20@users.noreply.github.com>
2022-07-21 10:13:02 +0900

    Update first_page.dart

--- lib/ui/main_screen_stack/second_page.dart
+++ lib/ui/main_screen_stack/second_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:travel_plan_app/design.dart';
 
 class SecondPage extends StatelessWidget {
